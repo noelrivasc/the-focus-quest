c687ed2cf29ff346f1ce264af5669ca81ee34c72
Noel Rivas <hola@noelr.dev>
2025-03-27 18:16:55 -0600

    Initial commit. Blog started from eleventy base blog.

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "TheFocus.Quest",
+	url: "https://thefocus.quest/",
+	language: "en",
+	description: "Noel Rivas' personal blog. I write about software development, wild critters found in the digital realm, and surrounding phenomena.",
+	author: {
+		name: "Noel Rivas",
+		email: "hola@noelr.dev",
+		url: "https://thefocus.quest/about-me/"
+	}
+}
